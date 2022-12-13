dbf3be1b1cc6b72d0496b449bdd2cdf428c7f9d8
angeliwanov <angel.iwanov@gmail.com>
2022-12-13 06:12:30 +0200

    video

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('success')
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// prompt user to select media, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // disable button
+    button.disabled = true;
+    // start picture in picture
+    await videoElement.requestPictureInPicture();
+    // reset button
+    button.disabled = false;
+})
+
+// Load
+selectMediaStream();
